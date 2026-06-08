b467e5b7e7fbe9deb2c480254707ba72385acf24
bishalr0y <bishalroy895@gmail.com>
2026-06-08 08:21:28 +0530

    feat: accepting user choice

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
@@ -11,8 +12,26 @@ func main() {
 		log.Fatalf("Error reading config: %v", err)
 	}
 
-	fmt.Println(config)
-	// TODO: let the user choose the split
+	fmt.Println("Welcome to toki!")
+	var choice int
+	for i, split := range config.Timers {
+		fmt.Printf("%d %v (Work: %v mins; Focus: %v mins)\n", i+1, split.Name, split.Focus, split.Break)
+	}
+
+	fmt.Println("Press 0 to quit")
+	fmt.Print("Enter your option >> ")
+	fmt.Scan(&choice)
+
+	switch {
+	case choice == 0:
+		fmt.Println("quiting...")
+		os.Exit(0)
+	case choice <= len(config.Timers):
+		fmt.Printf("split selected: %v\n", config.Timers[choice-1])
+	default:
+		fmt.Println("invalid option")
+		os.Exit(1)
+	}
 
 	// TODO: create a new split - open $EDITOR to create custom split
 	// then verify if the config is correct
