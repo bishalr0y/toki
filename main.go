accd67293710a73ab141a87978e5bc1a8d570b37
bishalr0y <bishalroy895@gmail.com>
2026-06-09 20:42:02 +0530

    wip

--- main.go
+++ main.go
@@ -14,34 +14,35 @@ func main() {
 	}
 
 	// TODO: create seperate files/functions to clean this mess up
-	BlueBoldUnderline.Println("Welcome to toki!")
+	BlueBoldUnderline.Println("Welcome to toki⏳")
 
-	var choice string
-	for i, split := range config.Timers {
-		fmt.Printf("%d %v (Work: %v mins; Focus: %v mins)\n", i+1, split.Name, split.Focus, split.Break)
-	}
+	for {
+		var choice string
+		for i, split := range config.Timers {
+			fmt.Printf("%d %v (🤓Focus: %v mins; 😎Break: %v mins)\n", i+1, split.Name, split.Focus, split.Break)
+		}
 
-	RedBold.Println("Press q to quit")
-	GreenBold.Print("Enter your option >> ")
-	fmt.Scan(&choice)
+		RedBold.Println("Press q to quit")
+		GreenBold.Print("Enter your option >> ")
+		fmt.Scan(&choice)
 
-	// TODO: maybe any other way to handle this better?
-	if choice == "q" {
-		fmt.Println("quitting...")
-		os.Exit(0)
-	}
+		if choice == "q" || choice == "Q" {
+			fmt.Println("goodbye👋🏻...")
+			os.Exit(0)
+		}
 
-	option, err := strconv.Atoi(choice)
-	if err != nil {
-		log.Fatalln("invalid choice")
-	}
+		option, err := strconv.Atoi(choice)
+		if err != nil {
+			log.Fatalln("invalid choice")
+		}
 
-	switch {
-	case option <= len(config.Timers):
-		fmt.Printf("split selected: %v\n", config.Timers[option-1])
-	default:
-		fmt.Println("invalid option")
-		os.Exit(1)
+		switch {
+		case option <= len(config.Timers):
+			fmt.Printf("split selected: %v\n", config.Timers[option-1])
+		default:
+			fmt.Println("invalid option")
+			os.Exit(1)
+		}
 	}
 
 	// TODO: create a new split - open $EDITOR to create custom split
